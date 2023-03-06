045502e572c0c2c648ed60bd2f5247861c1291d2
Erick Rojo <106215484+rojosecurity@users.noreply.github.com>
2023-03-06 08:24:27 -0600

    GenPass

--- genpass.py
+++ genpass.py
@@ -1,23 +1,28 @@
-#!/usr/bin/python3
+#!/usr/bin/python3 # invocando el interprete de python
 
+# importando modulos
 import argparse
 import string
 import random
 
-
-
+# generando clase 
 class GenPASS:
-    
+    # Metodo inial
     def __init__(self):
+    # definiendo los argumentos para invocar por consola
         parser = argparse.ArgumentParser(description = 'Generador de contraseñas')
+        # el argumento -l es el que va a definir la logitud del password
         parser.add_argument('-l',dest='longitudPasswd',help='numero de caracteres para su password')
         self.arguments = parser.parse_args()
-
-    def LongitudPassword(self):
-        for n in range(int(self.arguments.longitudPasswd)):
-            self.pwdMinima = random.choice(string.digits + string.punctuation + string.ascii_uppercase + string.ascii_lowercase)
-            print(self.pwdMinima,end="")
     
+    # Metodo clave
+    def LongitudPassword(self):
+        # iterar de acuerdo al valor ingresado por el argumento
+        for n in range(int(self.arguments.longitudPasswd)):
+            # generar la contraseña aleatoria en base las diferentes funciones
+            self.pwdMinima = random.choice(string.digits + string.punctuation + string.ascii_uppercase + string.ascii_lowercase)
+            # imprimir la contraseña de acuerdo a la iteración
+            print(self.pwdMinima,end="")
 
 # instancia de la clase
 getPasswd = GenPASS()
